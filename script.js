5a15b32966eff1a44ccdf15fc74c4bfab53bc2b0
Foverodigitech <72703128+Foverodigitech@users.noreply.github.com>
2020-12-18 01:21:58 +0100

    Almost done

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const ul = document.querySelector('.people');
+const buttons = document.querySelectorAll('button');
+console.log(buttons)
+const fullScreen = document.getElementsByClassName('floating-button');
+buttons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        button.classList.toggle('active');
+        let myEvent = button.dataset['command'];
+        console.log(myEvent)
+        document.execCommand(myEvent, false, null);
+    })
+})
 
-const people = ['Mario', 'Luigi', 'Ryu', 'Shaun', 'Chin-li'];
 
-html =``;
-people.forEach(function(person){
-    html+= `<li style="color: purple">${person}</li>`
-});
-
-console.log(html);
+fullScreen[0].addEventListener('click', ()=>{
+    let title = document.getElementsByClassName('title')[0];
+    let mainEditor = document.getElementsByClassName('text-editor-main')[0];
+    let outputContent = document.getElementsByClassName('contentOutput')[0];
+    title.classList.toggle('fullscreen-title');
+    mainEditor.classList.toggle('fullscreen-text-editor');
+    outputContent.classList.toggle('fullscreen-output');
+})
